60f53f89bc5622182e1b882ea53436550c39dcda
umairx25 <umairarhambd@gmail.com>
2025-06-30 18:27:38 -0400

    Started djan config

---
+++ backend/config/models.py
@@ -0,0 +1,11 @@
+# models.py
+from django.db import models
+
+class Task(models.Model): # each model is essentially a table
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
